e5f93673c47c421b1241e23e28aa15424f4fcfee
ninoko1995 <ninomiya.infratop@gmial.com>
2017-03-09 14:56:57 +0000

    make user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+	devise_for :users
+  resources :users,only: [:show,:edit,:update,:index]
   resources :books
   root 'home#top'
 
