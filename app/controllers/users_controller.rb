4f13beb73edebe891f3c8af8af409cf22db6d85b
ninoko1995 <ninomiya.infratop@gmial.com>
2017-03-09 15:18:52 +0000

    edit user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+  
   def show
+    @books = @user.books
   end
 
   def edit
@@ -13,4 +17,9 @@ class UsersController < ApplicationController
 
   def update
   end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
