14720c30a437ce0ecc93120faebefa7124f78d59
ninoko1995 <ninomiya.infratop@gmial.com>
2017-03-12 05:48:37 +0000

    finish favorite

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :log_in?
+
   def top
   	@name = 'Ninomiya'
   	@user = User.new
@@ -6,4 +8,11 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  private
+	  def log_in?
+	  	if !current_user.blank?
+	  		redirect_to user_path(current_user)
+	  	end
+	  end
 end
