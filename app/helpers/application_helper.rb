ed5d51857e74d3d7afafa47ea478d6954c2c976e
ninoko1995 <ninomiya.infratop@gmial.com>
2017-03-19 11:36:17 +0000

    little modefy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,17 @@ module ApplicationHelper
     end
   end
 
+
+  
+  def profile_url_thumb(user)
+    if user.image?
+      return user.image.thumb
+    else
+      return '/no-image-icon.jpg'
+    end
+  end
+
+
   def correct_user?(user)
   	if current_user.nil?
   		return false
